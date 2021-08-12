017010f8d34c5b274c2af9b0c765b67599f6b7af
Mvwestbury <67960396+Mvwestbury@users.noreply.github.com>
2021-08-12 15:19:48 +0200

    Update Coverage_calculation.sh

--- Coverage_calculation.sh
+++ Coverage_calculation.sh
@@ -1,16 +1,15 @@
 ## $1 Autosomes.bed
-## $2 Satsumaname
-## $3 X_trim.bed
-## $4 Output_folder
-## $5 Bamfile
-## $6 Output_prefix
+## $2 X_trim.bed
+## $3 Output_folder
+## $4 Bamfile
+## $5 Output_prefix
 
-samtools depth -a -q 25 -Q 25 -b $1 $5  > $4/$6_Autosomes_depth.txt
-samtools depth -a -q 25 -Q 25 -b $3 $5  > $4/$6_X_trim_depth.txt
+samtools depth -a -q 25 -Q 25 -b $1 $4  > $3/$5_Autosomes_depth.txt
+samtools depth -a -q 25 -Q 25 -b $2 $4  > $3/$5_X_trim_depth.txt
 for num in {1..10}
 do
-shuf -n10000000 $4/$6_Autosomes_depth.txt | awk '{sum=sum+$3}END{print sum/10000000}' >> $4/$6_Autosomes_bootstrap.txt
-shuf -n10000000 $4/$6_X_trim_depth.txt | awk '{sum=sum+$3}END{print sum/10000000}' >> $4/$6_X_trim_bootstrap.txt
+shuf -n10000000 $3/$5_Autosomes_depth.txt | awk '{sum=sum+$3}END{print sum/10000000}' >> $3/$5_Autosomes_bootstrap.txt
+shuf -n10000000 $3/$5_X_trim_depth.txt | awk '{sum=sum+$3}END{print sum/10000000}' >> $3/$5_X_trim_bootstrap.txt
 done
-paste $4/$6_Autosomes_bootstrap.txt $4/$6_X_trim_bootstrap.txt | awk  '{print $2/$1}' > $4/$6_ratios.txt
-rm $4/$6_Autosomes_depth.txt $4/$6_X_trim_depth.txt
+paste $4/$6_Autosomes_bootstrap.txt $3/$5_X_trim_bootstrap.txt | awk  '{print $2/$1}' > $3/$5_ratios.txt
+rm $3/$5_Autosomes_depth.txt $3/$5_X_trim_depth.txt
