f7a402c262bd74589ca6f68d9276268cf794004d
Mvwestbury <67960396+Mvwestbury@users.noreply.github.com>
2021-08-12 14:50:25 +0200

    Update SatsumaSynteny.sh

--- SatsumaSynteny.sh
+++ SatsumaSynteny.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-#1 - Threads
-#2 - Query sequence (genome of interest)
-#3 - Target sequence (Chromosomes)
-#4 - Output_directory
+## $1 - Threads
+## $2 - Query sequence (genome of interest)
+## $3 - Target sequence (Chromosomes)
+## $4 - Output_directory
+## $5 - Satsuma directory
 
-/groups/hologenomics/andreaac/Software/satsuma/SatsumaSynteny -m 1 -n $1 -q $2 -t $3 -o $4
+$5/SatsumaSynteny -m 1 -n $1 -q $2 -t $3 -o $4
