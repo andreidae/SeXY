4a599e875f9993b4bc8a54b12afa809d17db5748
Mick Westbury <67960396+Mvwestbury@users.noreply.github.com>
2022-03-11 17:21:52 +0100

    Update Coverage_calculation.sh

--- Coverage_calculation.sh
+++ Coverage_calculation.sh
@@ -11,5 +11,5 @@ do
 shuf -n10000000 $3/$5_Autosomes_depth.txt | awk '{sum=sum+$3}END{print sum/10000000}' >> $3/$5_Autosomes_bootstrap.txt
 shuf -n10000000 $3/$5_X_trim_depth.txt | awk '{sum=sum+$3}END{print sum/10000000}' >> $3/$5_X_trim_bootstrap.txt
 done
-paste $4/$6_Autosomes_bootstrap.txt $3/$5_X_trim_bootstrap.txt | awk  '{print $2/$1}' > $3/$5_ratios.txt
+paste $4/$5_Autosomes_bootstrap.txt $3/$5_X_trim_bootstrap.txt | awk  '{print $2/$1}' > $3/$5_ratios.txt
 rm $3/$5_Autosomes_depth.txt $3/$5_X_trim_depth.txt
